94d365ba1fdd2762457c6785523717c6017f438f
Juana <juanie@me.com>
2019-10-06 18:10:09 -0400

    this is new html and js

--- exercises/news-items/script.js
+++ exercises/news-items/script.js
@@ -1,24 +1,43 @@
 var ul = document.getElementsByTagName('ul')[0];
+var data = [];
 
-for (var i = 0; i < newsItems.length; i++) {
-  var item = newsItems[i];
+getData();
 
-  // Create list item
-  var li = document.createElement('li');
+async function getData(){
+  var url ='https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-06&sortBy=publishedAt&apiKey=981074e132db4680991359b7db4944b5';
+  var request = new Request(url);
+  var response = await fetch(request);
+  var responseBody = await response.json();
+  data = responseBody.articles;
+  addItemsToPage()
+  console.log(data);
+}
 
-  addThisToTheList('h4', item.date, li);
-  addThisToTheList('h6', item.author, li);
-  addThisToTheList('p', item.summary, li);
-  addThisToTheList('img', item.image, li);
+//https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-06&sortBy=publishedAt&apiKey=981074e132db4680991359b7db4944b5
 
-  // Add list item to list
-  ul.appendChild(li);
-};
 
-function addThisToTheList (type, text, container) {
+
+
+function addItemsToPage(){
+  for (var i = 0; i < data.length; i++) {
+    var item = data[i];
+
+    // Create list item
+    var li = document.createElement('li');
+
+    addThisToTheList('h4', item.publishedAt, li);
+    addThisToTheList('h6', item.author, li);
+    addThisToTheList('p', item.title, li);
+    addThisToTheList('img', item.urlToImage, li);
+
+    // Add list item to list
+    ul.appendChild(li);
+  };
+}
+function addThisToTheList (type, text, container)  {
   // Create the element
   var element = document.createElement(type);
-
+element.
   // Add some text to it
   if (type === 'img') {
     element.src = text;
@@ -29,7 +48,6 @@ function addThisToTheList (type, text, container) {
   // Add child to container
   container.appendChild(element);
 }
-
 
 
 
